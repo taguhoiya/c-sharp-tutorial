7cc21fd7e256996fbaf01b420a0fe4cc8bb72bf1
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-02-08 11:50:39 +0900

    helloworld

--- hello.cs
+++ hello.cs
@@ -1,8 +1,12 @@
 using System;
 public class HelloWorld
 {
-    static public void Main ()
-    {
-        Console.WriteLine ("Hello Mono World");
-    }
+  static public void Main()
+  {
+    Console.WriteLine("Hello Mono World");
+    string aFriend = "Bill";
+    Console.WriteLine(aFriend);
+    aFriend = "Maira";
+    Console.WriteLine(aFriend);
+  }
 }
