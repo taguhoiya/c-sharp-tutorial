69d1bff42a3261a505b205f919315c9baa7d0bc8
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-02-08 18:06:48 +0900

    format

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -1,16 +1,51 @@
-﻿var names = new List<string> { "<name>", "Ana", "Felipe" };
-foreach (var name in names)
+﻿WorkWithString();
+
+void WorkWithString()
 {
-  Console.WriteLine($"Hello {name.ToUpper()}!");
+  var names = new List<string> { "<name>", "Ana", "Felipe" };
+  foreach (var name in names)
+  {
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+  }
+
+  Console.WriteLine();
+  names.Add("Maria");
+  names.Add("Bill");
+  names.Remove("Ana");
+  foreach (var name in names)
+  {
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+  }
+
+  Console.WriteLine($"My name is {names[0]}");
+  Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+
+  Console.WriteLine($"The list has {names.Count} people in it");
+
+  var index = names.IndexOf("Felipe");
+  if (index == -1)
+  {
+    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+  }
+  else
+  {
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+  }
+
+  index = names.IndexOf("Not Found");
+  if (index == -1)
+  {
+    Console.WriteLine($"When an item is not found, IndexOf returns {index}");
+  }
+  else
+  {
+    Console.WriteLine($"The name {names[index]} is at index {index}");
+
+  }
+
+  names.Sort();
+  foreach (var name in names)
+  {
+    Console.WriteLine($"Hello {name.ToUpper()}!");
+  }
 }
-Console.WriteLine();
-names.Add("Maria");
-names.Add("Bill");
-names.Remove("Ana");
-foreach (var name in names)
-{
-  Console.WriteLine($"Hello {name.ToUpper()}!");
-}
-Console.WriteLine($"My name is {names[0]}");
-Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
-Console.WriteLine($"The list has {names.Count} people in it");
