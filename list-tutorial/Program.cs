5466a93a43a81e2694e3304ebaeaa870af16f8fa
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-02-08 17:51:52 +0900

    if

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var names = new List<string> { "<name>", "Ana", "Felipe" };
+foreach (var name in names)
+{
+  Console.WriteLine($"Hello {name.ToUpper()}!");
+}
+Console.WriteLine();
+names.Add("Maria");
+names.Add("Bill");
+names.Remove("Ana");
+foreach (var name in names)
+{
+  Console.WriteLine($"Hello {name.ToUpper()}!");
+}
