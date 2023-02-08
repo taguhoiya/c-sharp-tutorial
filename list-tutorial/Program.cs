868ccad9c10ab2a872d8361b0ef9871c3ed0fe84
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-02-08 18:00:12 +0900

    index

--- list-tutorial/Program.cs
+++ list-tutorial/Program.cs
@@ -11,3 +11,6 @@ foreach (var name in names)
 {
   Console.WriteLine($"Hello {name.ToUpper()}!");
 }
+Console.WriteLine($"My name is {names[0]}");
+Console.WriteLine($"I've added {names[2]} and {names[3]} to the list");
+Console.WriteLine($"The list has {names.Count} people in it");
