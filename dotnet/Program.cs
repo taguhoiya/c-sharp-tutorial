6fda417bb22a62e9cbdcd84da0401228b1d7c20b
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-02-08 13:08:38 +0900

    decimal

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,5 +1,5 @@
 ﻿// WorkWithIntegers();
-OrderPrecedence();
+Decimal();
 
 void WorkWithIntegers()
 {
@@ -42,4 +42,47 @@ void OrderPrecedence()
   int g = 3;
   int h = (e + f) / g;
   Console.WriteLine(h);
+}
+
+void Limitation()
+{
+  // int max = int.MaxValue;
+  // int min = int.MinValue;
+  // Console.WriteLine($"The range of integers is {min} to {max}");
+  // int what = max + 3;
+  // Console.WriteLine($"An example of overflow: {what}");
+
+  double a = 5;
+  double b = 4;
+  double c = 2;
+  double d = (a + b) / c;
+  Console.WriteLine(d);
+
+  double e = 19;
+  double f = 23;
+  double g = 8;
+  double h = (e + f) / g;
+  Console.WriteLine(h);
+
+  double max = double.MaxValue;
+  double min = double.MinValue;
+  Console.WriteLine($"The range of double is {min} to {max}");
+
+  double third = 1.0 / 3.0;
+  Console.WriteLine(third);
+}
+
+void Decimal()
+{
+  decimal min = decimal.MinValue;
+  decimal max = decimal.MaxValue;
+  Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+  double a = 1.0;
+  double b = 3.0;
+  Console.WriteLine(a / b);
+
+  decimal c = 1.0M;
+  decimal d = 3.0M;
+  Console.WriteLine(c / d);
 }
