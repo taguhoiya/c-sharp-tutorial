9353c5067bdda7b7f2ac50cba92403412d035eb2
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-02-09 13:06:10 +0900

    query

---
+++ type-dec/Program.cs
@@ -0,0 +1,31 @@
+﻿// // Declaration only:
+// float temperature;
+// string name;
+// // MyClass myClass;
+
+// // Declaration with initializers (four examples):
+// char firstLetter = 'C';
+// var limit = 3;
+// int[] source = { 0, 1, 2, 3, 4, 5 };
+
+class LowNums
+{
+  static void Main()
+  {
+    // A simple data source.
+    int[] numbers = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+    // Create the query.
+    // lowNums is an IEnumerable<int>
+    var lowNums = from num in numbers
+                  where num < 5
+                  select num;
+
+    // Execute the query.
+    foreach (int i in lowNums)
+    {
+      Console.Write(i + " ");
+    }
+  }
+}
+// Output: 4 1 3 2 0
