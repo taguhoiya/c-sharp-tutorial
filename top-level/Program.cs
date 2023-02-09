cd0cc26e76bc84e4e5a8d4ca4267c9c1e04d1b08
taguhoiya <kawasaki.takudai@moneyforward.co.jp>
2023-02-09 12:51:17 +0900

    top-level-argument

--- top-level/Program.cs
+++ top-level/Program.cs
@@ -1,22 +1,40 @@
 ﻿MyClass.TestMethod();
 MyNamespace.MyClass.MyMethod();
 
+if (args.Length > 0)
+{
+  foreach (var arg in args)
+  {
+    Console.WriteLine($"Argument={arg}");
+  }
+}
+else
+{
+  Console.WriteLine("No arguments");
+}
+
+string? s = Console.ReadLine();
+
+int returnValue = int.Parse(s ?? "-1");
+Console.WriteLine($"Argument={returnValue}");
+return returnValue;
+
 public class MyClass
 {
-    public static void TestMethod()
-    {
-        Console.WriteLine("Hello World!");
-    }
+  public static void TestMethod()
+  {
+    Console.WriteLine("Hello World!");
+  }
 
 }
 
 namespace MyNamespace
 {
-    class MyClass
+  class MyClass
+  {
+    public static void MyMethod()
     {
-        public static void MyMethod()
-        {
-            Console.WriteLine("Hello World from MyNamespace.MyClass.MyMethod!");
-        }
+      Console.WriteLine("Hello World from MyNamespace.MyClass.MyMethod!");
     }
+  }
 }
